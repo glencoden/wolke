6530c7a6fd608257bafd396089fe602bb2c07852
glen coden <simon.der.meyer@gmail.com>
2022-12-22 11:41:34 +0100

    validate docker build cache with most recent commit hash

---
+++ contexts/tsc/scripts/validate-cache.sh
@@ -0,0 +1,22 @@
+# Fetch remote commit info
+git_response=$(git ls-remote https://github.com/glencoden/tsc-api.git | egrep 'develop')
+# git_response=$(git ls-remote https://github.com/glencoden/happa.git | egrep 'main|merge')
+
+# Parse for commit hash
+parsed_response=$(echo "$git_response" | tr '\t' ' ')
+commit_hash=$(echo "$parsed_response" | cut -d' ' -f1)
+
+# Store the exit status of the git command in a variable
+git_exit_status=$?
+
+if [ $git_exit_status -eq 0 ]; then
+  echo "git command succeeded"
+  sed -i "" -e "6s/.*/ARG COMMIT_HASH=$commit_hash/" contexts/tsc/Dockerfile
+else
+  echo "git command failed"
+fi
+
+# If the commit hash is no option, you can invalidate docker cache every run with a timestamp:
+
+# cache_time=$(date +%s)
+# sed -i "" -e "6s/.*/ARG CACHE_TIME=$cache_time/" contexts/tsc/Dockerfile
--- scripts/set-cache-time.sh
+++
@@ -1,3 +0,0 @@
-cache_time=$(date +%s)
-
-sed -i "" -e "6s/.*/ARG CACHE_TIME=$cache_time/" contexts/tsc/Dockerfile
